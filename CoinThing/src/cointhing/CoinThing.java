c4a9487c3d51329abb8b70ef292b449b985bf73f
Sam Luther <samluther998@gmail.com>
2018-04-26 17:55:31 -0600

    Update CoinThing.java

--- CoinThing/src/cointhing/CoinThing.java
+++ CoinThing/src/cointhing/CoinThing.java
@@ -1,7 +1,7 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Sam Luther & Matt Wintersteen
+ * Programming Assignment 4
+ * Tues April 24th
  */
 package cointhing;
 
